c51328b5112bd74da142e773ee9ba059d4ed46e2
Ced-Mich Pro-Tail <37204120+ced210@users.noreply.github.com>
2020-08-23 11:48:45 -0400

    fix router link list, fix App layout

--- character-book/src/App.vue
+++ character-book/src/App.vue
@@ -6,55 +6,45 @@
         color="primary"
         dark
         app
-        absolute
         collapse-on-scroll
-        scroll-target="#scrolling-techniques-2"
+        scroll-target="#scrolling-techniques-1"
       >
         <v-app-bar-nav-icon @click="drawer = true" />
         <v-toolbar-title v-text="'Character Book'"></v-toolbar-title>
       </v-app-bar>
-      <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-navigation-drawer v-model="drawer" temporary app>
         <v-list nav dense>
           <v-list-item-group
             v-model="group"
             active-class="primary--text text--secondary"
           >
-            <router-link to="/">
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>home</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Home</v-list-item-title>
-              </v-list-item>
-            </router-link>
-            <router-link to="/character-creation">
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>face</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>Character Creation</v-list-item-title>
-              </v-list-item>
-            </router-link>
-            <router-link to="/about">
-              <v-list-item>
-                <v-list-item-icon>
-                  <v-icon>info</v-icon>
-                </v-list-item-icon>
-                <v-list-item-title>About</v-list-item-title>
-              </v-list-item>
-            </router-link>
+            <v-list-item to="/">
+              <v-list-item-icon>
+                <v-icon>home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="/character-creation">
+              <v-list-item-icon>
+                <v-icon>face</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Character Creation</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="/about">
+              <v-list-item-icon>
+                <v-icon>info</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>About</v-list-item-title>
+            </v-list-item>
           </v-list-item-group>
         </v-list>
       </v-navigation-drawer>
-      <v-sheet
-        id="scrolling-techniques-2"
-        class="overflow-y-auto mt-5"
-        max-height="600"
-      >
-        <v-container style="height: 1500px;" class="mt-5">
+      <v-main>
+        <v-container id="scrolling-techniques-1" fluid>
           <router-view />
         </v-container>
-      </v-sheet>
+      </v-main>
+      <v-footer app> </v-footer>
     </v-app>
   </div>
 </template>

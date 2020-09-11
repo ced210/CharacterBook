f72c5be545d5efc231cdd2fd33f4fea6c854c237
Ced-Mich Pro-Tail <37204120+ced210@users.noreply.github.com>
2020-09-10 23:10:12 -0400

    Set Up Sequilizer, CRUD routes Example

--- app/app/server.js
+++ app/app/server.js
@@ -16,10 +16,19 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./models");
+// db.sequelize.sync();
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
+
+require("./routes/turorial.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;

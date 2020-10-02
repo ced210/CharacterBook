af5f3c4ff730b8a28b85c43d402e7f531bb5ef7c
Ced-Mich Pro-Tail <37204120+ced210@users.noreply.github.com>
2020-10-01 22:31:41 -0400

    Setup Prod

--- character-book/server/config/db.config.js
+++ character-book/server/config/db.config.js
@@ -1,9 +1,22 @@
+process.env.NODE_ENV = process.env.NODE_ENV || "dev";
+
+let HOST = "localhost";
+let USER = "root";
+if (process.env.NODE_ENV === "test") {
+} else if (process.env.NODE_ENV === "prod") {
+  HOST = "characterbook.mysql.database.azure.com";
+  USER = "admin210@characterbook";
+} else if (process.env.NODE_ENV === "dev") {
+  HOST = "localhost";
+  USER = "root";
+}
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
+  HOST: HOST,
+  USER: USER,
   PASSWORD: "!Book!17",
   DB: "characterbook",
   dialect: "mysql",
+  port: 3306,
   pool: {
     max: 5,
     min: 0,
